1add17ad06305cc6947c6a0e780a023583eb24f9
Youngjae Choi <cdw0807@naver.com>
2022-06-27 15:16:13 +0900

    Initial Commit

---
+++ HeadSpaceFocus/QuickFocusListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  QuickFocusListViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by 최영재 on 2022/06/27.
+//
+
+import UIKit
+
+class QuickFocusListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
