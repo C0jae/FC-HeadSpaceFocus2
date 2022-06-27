45e3d131859a0b6570197ce2dfb86015e49629b5
Youngjae Choi <cdw0807@naver.com>
2022-06-27 15:51:19 +0900

    강의2

--- HeadSpaceFocus/QuickFocusListViewController.swift
+++ HeadSpaceFocus/QuickFocusListViewController.swift
@@ -8,9 +8,54 @@
 import UIKit
 
 class QuickFocusListViewController: UIViewController {
-
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let breathingList = QuickFocus.breathing
+    let walkingList = QuickFocus.walking
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    typealias Item = QuickFocus
+    enum Section: CaseIterable {
+        case breathe
+        case walking
+        
+        var title: String {
+            switch self {
+            case .breathe: return "Breathing exercises"
+            case .walking: return "Mindful walks"
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "QuickFocusCell", for: indexPath) as? QuickFocusCell else { return nil }
+            
+            cell.configure(item)
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.breathe, .walking])
+        snapshot.appendItems(breathingList, toSection: .breathe)
+        snapshot.appendItems(walkingList, toSection: .walking)
+        datasource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
     }
 
+    private func layout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .estimated(50))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
 }
